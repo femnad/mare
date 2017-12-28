bd7f1153d51a40a2310bf7922b9b431b9acf01ec
femnad <commit_person@f-m.fm>
2017-12-28 18:40:24 +0000

    Initial Commit!

---
+++ mare.go
@@ -0,0 +1,57 @@
+package mare
+
+import (
+	"os"
+	"strings"
+)
+
+const (
+	homeEnvVar = "HOME"
+	tilde      = "~"
+)
+
+func PanicIfErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ExpandUser(path string) string {
+	home := os.Getenv(homeEnvVar)
+	return strings.Replace(path, tilde, home, 1)
+}
+
+func Filter(items []string, f func(string) bool) []string {
+	matchingItems := make([]string, 0)
+	for _, item := range items {
+		if f(item) {
+			matchingItems = append(matchingItems, item)
+		}
+	}
+	return matchingItems
+}
+
+func Map(items []string, f func(string) string) []string {
+	outputItems := make([]string, len(items))
+	for index, item := range items {
+		outputItems[index] = f(item)
+	}
+	return outputItems
+}
+
+func FlatMap(items []string, f func(string) []string) []string {
+	outputItems := make([]string, 0)
+	for _, item := range items {
+		outputList := f(item)
+		outputItems = append(outputItems, outputList...)
+	}
+	return outputItems
+}
+
+func MapFileInfo(items []os.FileInfo, f func(os.FileInfo) string) []string {
+	outputItems := make([]string, len(items))
+	for index, item := range items {
+		outputItems[index] = f(item)
+	}
+	return outputItems
+}
