1cb000db21f0cadb7b7945d5b2cb75fc77b21621
fcd <1352288+femnad@users.noreply.github.com>
2024-02-07 19:41:14 +0000

    don't try to create dir with empty name

--- dir.go
+++ dir.go
@@ -4,6 +4,10 @@ import "os"
 
 // EnsureDir makes sure that the given directory exists.
 func EnsureDir(dir string) error {
+	if dir == "" {
+		return nil
+	}
+
 	_, err := os.Stat(dir)
 	if os.IsNotExist(err) {
 		err = os.MkdirAll(dir, 0744)
